bd9b14854a43604160405099c8dede7317a55a16
Nixon <kiplangatnixon28@gmail.com>
2024-09-11 16:34:49 +0300

    opens files in python

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
 def write_file(file_name, file_content):
-    pass
+    if not str(file_name).endswith('.txt'):
+        file_name = f"{file_name}.txt"
+
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    if not str(file_name,).endswith('.txt'):
+        file_name = f"{file_name}.txt"
+        
+
+    with open(file_name, 'a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    if not str(file_name).endswith('.txt'):
+        file_name = f"{file_name}.txt"
+
+
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read()
